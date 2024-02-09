36d3a5a241891bbd527721dcc14be6a6118ba0dc
xarukibuu21 <khapkhaan@gmail.com>
2024-02-09 15:16:53 +0300

    release find by userId

--- src/routes/itemRouter.js
+++ src/routes/itemRouter.js
@@ -3,15 +3,23 @@ import { Item } from '../../db/models';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try {
-    const items = await Item.findAll();
-    const initState = { items };
-    res.render('ItemPage', initState);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-});
+router
+  .get('/', async (req, res) => {
+    // const userId = req.params.id;
+    try {
+      const items = await Item.findAll();
+      // {
+      //   where: {
+      //     userId: res.locals.user.id,
+      //   },
+      // }
+      console.log('111111', req.params.id, '1111111');
+      const initState = { items };
+      res.render('ItemPage', initState);
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  });
 
 export default router;
